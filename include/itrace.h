9b53fe6619a92aa2bc6dc0d344b23a70349dee1a
Jim Clause <clause@cc.gatech.edu>
2009-09-16 17:02:37 -0400

    Added infrastructre to support register peeking

--- include/itrace.h
+++ include/itrace.h
@@ -2,15 +2,23 @@
 #define __ITRACE_H__
 
 #include <stdint.h>
+#include <sys/user.h>
+
+typedef struct _itrace_t itrace_t;
+
+typedef enum _reg_t reg_t;
 
 typedef void (*peek_mem_func_t)(uint8_t* buf, size_t cnt, off_t addr);
+typedef uint32_t (*peek_reg_func_t)(reg_t reg);
 
-typedef struct itrace_t *itrace_handle;
 
-itrace_handle itrace_init();
-void itrace_free(itrace_handle trace);
 
-void itrace_set_peek_mem_func(itrace_handle trace, peek_mem_func_t f);
-void itrace_trace(itrace_handle trace, uint32_t eip);
+extern itrace_t *itrace_init();
+extern void itrace_free(itrace_t *trace);
+
+extern void itrace_set_peek_mem_func(itrace_t *trace, peek_mem_func_t f);
+extern void itrace_set_peek_reg_func(itrace_t *trace, peek_reg_func_t f);
+
+extern void itrace_trace(itrace_t *trace, uint32_t eip);
 
 #endif
--- lib/itrace.c
+++ lib/itrace.c
@@ -5,15 +5,16 @@
 #include "itrace.h"
 #include "udis86.h"
 
-struct itrace_t {
+struct _itrace_t {
 	ud_t *ud_obj;	
 	peek_mem_func_t peek_mem;
+	peek_reg_func_t peek_reg;
 };
 
-itrace_handle itrace_init() {
-	struct itrace_t *trace;
+itrace_t *itrace_init() {
+	itrace_t *trace;
 
-	trace = (struct itrace_t *) malloc(sizeof(*trace));
+	trace = (itrace_t *) malloc(sizeof(*trace));
 	assert(trace);
 
 	trace->ud_obj = (ud_t *) malloc(sizeof(*(trace->ud_obj)));
@@ -25,16 +26,20 @@ itrace_handle itrace_init() {
         return trace;
 }
 
-void itrace_free(itrace_handle trace) {
+void itrace_free(itrace_t *trace) {
 	free(trace->ud_obj);
 	free(trace);
 }
 
-void itrace_set_peek_mem_func(itrace_handle trace, peek_mem_func_t f) {
+void itrace_set_peek_reg_func(itrace_t *trace, peek_reg_func_t f) {
+	trace->peek_reg = f;
+}
+
+void itrace_set_peek_mem_func(itrace_t *trace, peek_mem_func_t f) {
 	trace->peek_mem = f;
 }
 
-void itrace_trace(itrace_handle trace, uint32_t eip) {
+void itrace_trace(itrace_t *trace, uint32_t eip) {
 	uint8_t code[12];
 	trace->peek_mem(code, sizeof(code), eip);	
 
